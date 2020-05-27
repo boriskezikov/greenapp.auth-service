eaf2936a24c4e240b452de57011a24f2f1cb1173
boriskezikov <boris200898@icloud.com>
2020-05-27 22:32:01 +0300

    routes

--- src/main/java/com/greenapp/authservice/AuthServiceApplication.java
+++ src/main/java/com/greenapp/authservice/AuthServiceApplication.java
@@ -2,14 +2,18 @@ package com.greenapp.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AuthServiceApplication.class, args);
     }
-
 }
