432a45f288825c6cca60cd27a05169b997196841
boriskezikov <boris200898@icloud.com>
2020-06-01 23:19:52 +0300

    fix

--- src/main/java/com/greenapp/authservice/api/AuthController.java
+++ src/main/java/com/greenapp/authservice/api/AuthController.java
@@ -3,6 +3,8 @@ package com.greenapp.authservice.api;
 import com.greenapp.authservice.dto.UserInfo;
 import com.greenapp.authservice.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +17,16 @@ import javax.validation.constraints.NotNull;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final Logger logger = LoggerFactory.getLogger(AuthController.class.getName());
+
     private final UserService service;
 
     @GetMapping
     public UserInfo authenticateUser(@RequestParam @NotNull String mail) {
         var user = service.findUserByMail(mail);
+        logger.warn("__________________________________________________");
+        logger.warn(" Mail to authenticate: " + mail);
+        logger.warn("__________________________________________________");
         return UserInfo.builder()
                 .username(user.getMailAddress())
                 .password(user.getPassword())
