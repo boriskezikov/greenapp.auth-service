d5bd19bdd7e7c32571d7af1da6a90d7774e3bd61
boriskezikov <boris200898@icloud.com>
2020-06-25 01:31:20 +0300

    Revert: connect auth and client (867bd145)

--- src/main/java/com/greenapp/authservice/kafka/MailTopics.java
+++ src/main/java/com/greenapp/authservice/kafka/MailTopics.java
@@ -5,5 +5,6 @@ import static com.greenapp.authservice.kafka.KafkaConfigConstants.TOPIC_PREFIX;
 
 public interface MailTopics {
     String MAIL_2FA_TOPIC = TOPIC_PREFIX + "mail-auth2fa-account-created";
-    String CLIENT_CREATED_TOPIC = TOPIC_PREFIX + "client-created-topic";
+    String PASSWORD_RESET = TOPIC_PREFIX + "password-reset";
+
 }
